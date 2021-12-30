a94c9bc48cb802adaf7d73d0eab1b404c4ecfb89
Devarzia Barkins <devarziabarkins@outlook.com>
2021-12-30 01:10:00 -0500

    Modified code to change the layout of the Calculator project. Need to refactor the bubbling effect page

---
+++ RandomProjects/wwwroot/js/BubblingTable.js
@@ -0,0 +1,89 @@
+﻿// To Handle all targets with the classname "select"
+window.addEventListener("load", function () {
+    var table = element("Bubbling");
+    table.addEventListener("change", function (event) {
+        var target = event.target;
+        if (target.className == "select") {
+            if (target.checked) {
+                target.parentNode.parentNode.className = "select";
+            }
+            else {
+                target.parentNode.parentNode.className = "";
+            }
+        }
+    });
+});
+
+//// Getting all checkboxes with if statement
+//window.addEventListener("load", function ()
+//{
+//    var checkboxContainer = element("checkboxTable");
+//    checkboxContainer.addEventListener("click", function(event)
+//    {
+//        var target = event.target;
+//        if (target.nodeName == "INPUT")
+//        {
+//            if (element("chk1").checked)
+//            {
+//                element("chk1").parentNode.parentNode.className = "firstRowColor";
+//            }
+//            else
+//            {
+//                element("chk1").parentNode.parentNode.className = "firstRowClearColor";
+//            }
+
+//            if (element("chk2").checked)
+//            {
+//                element("chk2").parentNode.parentNode.className = "secondRowColor";
+//            }
+//            else
+//            {
+//                element("chk2").parentNode.parentNode.className = "secondRowClearColor";
+//            }
+
+//            if (element("chk3").checked)
+//            {
+//                element("chk3").parentNode.parentNode.className = "thirdRowColor";
+//            }
+//            else
+//            {
+//                element("chk3").parentNode.parentNode.className = "thirdRowClearColor";
+//            }
+//        }
+//    });
+//});
+
+////Function that calls to the 
+//function highlightRow()
+//{
+//    if (element("chk1").checked)
+//    {
+//        element("chk1").parentNode.parentNode.className = "firstRowColor";
+//    }
+//    else 
+//    {
+//        element("chk1").parentNode.parentNode.className = "firstRowClearColor";
+//    }
+
+//    if (element("chk2").checked)
+//    {
+//        element("chk2").parentNode.parentNode.className = "secondRowColor";
+//    }
+//    else
+//    {
+//        element("chk2").parentNode.parentNode.className = "secondRowClearColor";
+//    }
+
+//    if (element("chk3").checked)
+//    {
+//        element("chk3").parentNode.parentNode.className = "thirdRowColor";
+//    }
+//    else
+//    {
+//        element("chk3").parentNode.parentNode.className = "thirdRowClearColor";
+//    }
+//}
+
+function element(id) {
+    return document.getElementById(id)
+}
---
+++ RandomProjects/wwwroot/js/Calculator.js
@@ -0,0 +1,53 @@
+﻿// Creating an event listener for the page
+// Excutes when the page load
+window.addEventListener("load", function () {
+    // Grabbing the container for the buttons
+    var buttonContainer = but("operations");
+
+    // Adding an event listener for the buttons
+    // Excutes when a button in the container is clicked
+    buttonContainer.addEventListener("click", function (event) {
+        // Setting a target variable 
+        var target = event.target;
+
+        // If Statement: If the target nodeName equal to input
+        // the calculation will be made based on the case stated below
+        if (target.nodeName == "INPUT") {
+            var firstNumber = but("txtFirstNumber").value ? parseInt(but("txtFirstNumber").value) : NaN;
+            var secondNumber = but("txtSecondNumber").value ? parseInt(but("txtSecondNumber").value) : NaN;
+            var result;
+            var output = but("Result");
+
+            switch (target.value) {
+                case "Add":
+                    result = firstNumber + secondNumber;
+                    break;
+
+                case "Subtract":
+                    result = firstNumber - secondNumber;
+                    break;
+
+                case "Multiply":
+                    result = firstNumber * secondNumber;
+                    break;
+
+                case "Divide":
+                    result = Math.round(firstNumber / secondNumber);
+                    break;
+            }
+
+            // If statement: If the result isn't null
+            // display results
+            if (result != "NaN") {
+                output.className = "";
+                output.innerHTML = "Result: " + result;
+                txtFirstNumber.focus();
+            }
+        }
+    });
+});
+
+// function that returns the element by ID
+function but(id) {
+    return document.getElementById(id)
+}
