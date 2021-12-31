a085eb5e0b3eb0b14d687d251d97470be67a976e
Devarzia Barkins <devarziabarkins@outlook.com>
2021-12-31 00:29:10 -0500

    Modifed code to convert to JavaScript projects to jQuery.
    AngularJS projects are not functional at this time

---
+++ RandomProjects/wwwroot/js/GetMovieData.js
@@ -0,0 +1,30 @@
+﻿// Declaring The angularjs app
+var movieApp = angular.module('movieApp', []);
+var facebookApp = angular.module('FacebookApp', []);
+
+// Creating the controller for the movieApp
+movieApp.controller('Movie', function GetMovieData($scope, $http, $window) {
+    // Declaring the function and passing in the title parameter
+    $scope.movie = function (title) {
+        // The Url for the data and title (from website)
+        var URI = 'https://www.omdbapi.com/?t=' + title;
+        $http.get(URI)
+            .success(function (MovieData) {
+                var outputString = "";
+                for (var key in MovieData) {
+                    outputString += key + " : " + MovieData[key] + '\n';
+                }
+
+                // Parsing the data to the output div
+                $scope.MovieInfo = outputString;
+            });
+    };
+
+    $scope.MovieDatabase = function (url) {
+        $window.open(url);
+    };
+});
+
+facebookApp.controller("FacebookAppController", ["$scope", function ($scope) {
+    $scope.ID = data;
+}]);
--- RandomProjects/wwwroot/js/site.js
+++ RandomProjects/wwwroot/js/site.js
@@ -2,3 +2,54 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function () {
+    var table = $("#Bubbling");
+    table.on("change", function (event) {
+        var target = event.target;
+        if (target.className == "select") {
+            if (target.checked) {
+                target.parentNode.parentNode.className = "select";
+            }
+            else {
+                target.parentNode.parentNode.className = "";
+            }
+        }
+    });
+
+    var buttonContainer = $("#operations");
+    buttonContainer.on("click", function (event) {
+        var target = event.target;
+
+        if (target.nodeName == "INPUT") {
+            var firstNumber = $("#txtFirstNumber").val() ? parseInt($("#txtFirstNumber").val()) : NaN;
+            var secondNumber = $("#txtSecondNumber").val() ? parseInt($("#txtSecondNumber").val()) : NaN;
+            var result;
+            var output = $("#Result");
+
+            switch (target.value) {
+                case "Add":
+                    result = firstNumber + secondNumber;
+                    break;
+
+                case "Subtract":
+                    result = firstNumber - secondNumber;
+                    break;
+
+                case "Multiply":
+                    result = firstNumber * secondNumber;
+                    break;
+
+                case "Divide":
+                    result = Math.round(firstNumber / secondNumber);
+                    break;
+            }
+            alert(result);
+            // If statement: If the result isn't null
+            // display results
+            if (result != "NaN") {
+                output.text("Result: " + result);
+                $("#txtFirstNumber").focus();
+            }
+        }
+    })
+});
